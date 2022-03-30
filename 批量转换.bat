10f48e7020d6405be3f63d68b8284dc87616d2da
系统远程维护工作室 <94363939+Control1980@users.noreply.github.com>
2022-03-30 15:56:16 +0800

    Add files via upload

---
+++ 批量转换.bat
@@ -0,0 +1,12 @@
+@echo off
+::在下方设置要处理的视频或音频格式，这里列出了一些主要的视频格式
+set Ext=*.avi,*.mp4,*.wmv,*.flv,*.mkv,*.rmvb,*.rm,*.3gp,*.ts
+md output
+echo 开始视频转换
+::在下方设置输出格式，这里输出为mp4，可自行更改
+for %%a in (%Ext%) do (
+	echo 正在转换：%%a
+	ffmpeg -loglevel quiet -i "%%a" -f mp4 -c copy "output\%%~na.mp4" -y
+)
+echo 转换完成
+pause
